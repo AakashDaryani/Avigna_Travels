e7f184576eb6222e91202b1e3ad73959ceb6b705
Aakash Daryani <aakashdaryani50@gmail.com>
2023-05-25 18:15:41 +0530

    Third commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const nodemailer = require("nodemailer");
 const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const cors = require('cors');
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 let url = 'mongodb+srv://aakashdaryani50:BjfnRRtEz6lHeJKr@cluster0.vq2uhqc.mongodb.net/'
 mongoose
